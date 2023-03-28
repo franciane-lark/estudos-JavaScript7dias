3ef70e191b306baf2b8cf33e9ba97263c9d15048
Franciane Lark <franciane.lark019@gmail.com>
2023-03-27 21:27:55 -0400

    update comparacao e ternario

---
+++ Dia02/04-script.js
@@ -0,0 +1,14 @@
+let number1 = 1;
+let number2 = 1;
+
+console.log(number1 === number2);
+//Resposta: True
+
+
+let num1 = '1';
+let num2 = 1;
+
+console.log(num1 === num2);
+//Resposta: False
+console.log(num1 == num2);
+//Resposta: True
